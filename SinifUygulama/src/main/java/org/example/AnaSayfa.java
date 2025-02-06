5d0b50e1d322d3b41601d69dee007ca40f8ee24a
Arif <arifalihan2016@gmail.com>
2025-02-06 18:39:39 +0300

    profil butonu eklendi

--- SinifUygulama/src/main/java/org/example/AnaSayfa.java
+++ SinifUygulama/src/main/java/org/example/AnaSayfa.java
@@ -4,10 +4,16 @@ import javax.swing.*;
 
 public class AnaSayfa {
     JFrame frame = new JFrame();
-
+    JButton button = new JButton("Profile göster");
     AnaSayfa(){
+        button.setBounds(200,200,100,50);
+        button.setFocusable(false);
+        frame.add(button);
+
         frame.setSize(500,500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null );
         frame.setVisible(true);
+
     }
 }
